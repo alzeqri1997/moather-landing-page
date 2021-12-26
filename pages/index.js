4c976d317bacbcf39efbb536d4f60bdbd8ead139
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-26 11:38:23 +0300

    add Main Layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../layouts/Header";
+import Main from "../layouts/Main";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Main />
     </>
   );
 }
