9186c6317ddd18f130fe9d9837213f3739a47670
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-26 10:35:30 +0300

    Starter Code

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "36px",
+        sm: "100px",
+        lg: "140px",
+        xl: "140px",
+        "2xl": "140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        "shamel-bold": ["shamel-bold", "sans-serif"],
+        "shamel-book": ["shamel-book", "sans-serif"],
+        "shamel-light": ["shamel-light", "sans-serif"],
+        "shamel-medium": ["shamel-medium", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
