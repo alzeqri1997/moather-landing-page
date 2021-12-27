3983e929ed6efb9abfaddf5789d141d8292ab014
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-12-27 09:47:07 +0300

    add slider data

---
+++ components/ImageSlider/SliderData.jsx
@@ -0,0 +1,20 @@
+import img1 from '../../public/images/image-slider/1.png'
+import img2 from '../../public/images/image-slider/2.png'
+import img3 from '../../public/images/image-slider/3.png'
+import img4 from '../../public/images/image-slider/4.png'
+
+
+export const SliderData = [
+  {
+    image: img4.src
+  },
+  {
+    image: img3.src
+  },
+  {
+    image: img2.src
+  },
+  {
+    image: img1.src
+  },
+]
